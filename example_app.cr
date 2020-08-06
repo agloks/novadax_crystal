abed901e65a64031ac9abb3a698a6b37da8fd550
agloks <smoke.day@yahoo.com>
2020-08-06 12:33:51 -0300

    add: Signature to get requests

--- example_app.cr
+++ example_app.cr
@@ -1,7 +1,7 @@
 require "./src/depedency"
 require "./env"
 
-novadax = NovadaxCrystal::NovadaxAPI.new
+novadax = NovadaxCrystal::NovadaxAPI.new ENV["ACESS_KEY"], ENV["SECRET_KEY"]
 request = novadax.getRecentTrades("LTC_BRL", "2")
 data = RecentTrades.from_json(request).to_h
 
