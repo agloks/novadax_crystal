9a60fb473e79475b03b6718e81a19e635c1df4a4
agloks <smoke.day@yahoo.com>
2020-08-13 20:48:20 -0300

    add: env with params to api constructor

--- example_app.cr
+++ example_app.cr
@@ -2,7 +2,7 @@ require "./src/depedency"
 require "./env"
 
 novadax = NovadaxCrystal::NovadaxAPI.new ENV["ACESS_KEY"], ENV["SECRET_KEY"]
-request = novadax.getRecentTrades("LTC_BRL", "2")
+request = novadax.getRecentTrades("LTC_BRL", "5")
 data = RecentTrades.from_json(request).to_h
 
 p data
